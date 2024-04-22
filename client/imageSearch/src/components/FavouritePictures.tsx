aa8433d7ed3dfd9357dfaa4fd05cb5dcfda93ab4
fabian0270 <fabianolsson28@gmail.com>
2024-04-22 23:16:28 +0200

    fixed server

--- client/imageSearch/src/components/FavouritePictures.tsx
+++ client/imageSearch/src/components/FavouritePictures.tsx
@@ -1,57 +1,42 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import React, { useState, useEffect } from 'react';
-//import { IUser } from '../models/IUser';
 
-/*interface IUserProps {
-    user: IUser;
-}*/
 
 const FavouritePictures = () => {
     const { user } = useAuth0();
     const [favorites, setFavorites] = useState([]);
     const [isReady, setIsReady] = useState(false);
-    const [userId, setUserId] = useState('');
 
     // Funktion för att hämta användarens favoritbilder från servern  
     const fetchFavorites = async () => {
         if (user?.sub) {
-            console.log('SUB', user); 
-           setUserId(user?.sub);   
-           console.log('user id: ', userId);
-           if (userId) {   
+            console.log('SUB', user.sub); 
+             
             try {       
-                const response = await fetch(`http://localhost:5172/favorite/${userId}`);
-                if (response.ok) {
-                    const data = await response.json(); 
-                    setFavorites(data.favorite_images); 
-                    console.log('favorites: ', favorites);
-                    if (favorites.length > 0) {
-                        setIsReady(true); 
-                    }
-                     
-                } else {
-                    console.error('Failed to fetch favorites:', response.statusText);
+                const response = await fetch(`http://localhost:5172/favorite/${user.sub}`);
+                const data = await response.json(); 
+                
+                setFavorites(await data.favorite_images); 
+                if (data.favorite_images) {
+                    setIsReady(true);  
                 }
+  
             } catch (error) {
                 console.error('Error fetching favorites:', error);
             }            
-           } else {
-            return;
-           }
-
 
         } else if(user?.sub === '') {
             console.log('NO SUB', user);
             
             return;
         }  
-
+        console.log('favorites: ', favorites);
     };
 
     // Anropa fetchFavorites funktionen när komponenten renderas för första gången
     useEffect(() => {
         fetchFavorites();
-    }, [user]); // Uppdatera favoritlistan när användaren ändras
+    }, [user?.sub]); // Uppdatera favoritlistan när användaren ändras
 
 
     return (
